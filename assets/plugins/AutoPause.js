8adc2ce028014d57be86d2e1fdb2d4a065577c75
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-08-29 11:06:44 -0500

    Se agrego el visibilitychange para evitar que nuestro video se siga reproduciendo si el usuario cambio de pestaña en el navegador.

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
     constructor() {
         this.threshold = 0.45;
         this.handleIntersection = this.handleIntersection.bind(this)
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
     }
     run(player){
         this.player = player
@@ -9,6 +10,7 @@ class AutoPause {
             threshold: this.threshold
         });
         observer.observe(this.player.media)
+        document.addEventListener("visibilitychange", this.handleVisibilityChange)
     }
     handleIntersection(entries) {
         const entry = entries[0];
@@ -20,6 +22,16 @@ class AutoPause {
             this.player.pause();
         }
         
+    }
+    handleVisibilityChange(){
+        const isvisible = document.visibilityState === "visible"
+        console.log(isvisible)
+        if(isvisible){
+            this.player.play()
+        } else {
+            this.player.pause()
+        }
+
     }
 }
 
